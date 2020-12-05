b7041c695e1620e5e7cecb5125e16523ac93dc31
Joel Alabi <joelalabi45@gmail.com>
2020-12-05 13:45:27 +0000

    started screens

---
+++ AddToCart.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import { StyleSheet, Text, View,Image,TouchableOpacity,} from 'react-native';
+
+
+
+export default function AddToCart() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.pageTitleInfoContainer}>
+                    <Text style={styles.heading}>Add To Cart</Text>
+                    <Text style={styles.paragraph}> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                             when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                             It has survived not only five centuries, but also the leap into electronic typesetting,
+                              remaining essentially unchanged.
+                    </Text>
+                 </View>
+                 <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("./assets/undraw_add_to_cart_vkjp.png")}
+        />
+      </View>       
+                     
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Next</Text>
+      </TouchableOpacity>
+      <View style={styles.footer}>
+        <Text
+          
+          style={styles.footerSkip}
+        >
+          Previous
+        </Text>
+
+        <Text
+          
+          style={styles.footerPrevious}
+        >
+          Skip
+        </Text>
+      </View>
+        </View>
+    )
+}
+const styles= StyleSheet.create({
+    container:{
+        flex:1,
+        flexDirection:'column',
+        marginVertical:35,
+    },
+    pageTitleInfoContainer:{
+        flexDirection:'column',
+        marginTop:40,
+        
+    },
+    heading:{
+        fontSize:23,
+        textTransform:'uppercase',
+        fontWeight:'bold',
+        marginBottom:10
+    },
+    paragraph:{
+        color:'#707070',
+        fontSize:15,
+        textAlign:'left',
+},
+image: {
+
+    height:230,
+    width:230,
+    
+  },
+  imageContainer: {
+    alignItems: "center",
+    marginTop:30,
+  },
+  button: {
+    backgroundColor: "#8132a8",
+    height: 50,
+    width: 120,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 30,
+    alignSelf: "center",
+    marginTop: 25,
+  },
+
+  buttonText: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  footer: {
+    flexDirection: "row",
+     marginTop: 50,
+    justifyContent: "space-between",
+  },
+
+  footerSkip: {
+    color: "#a3a3a3",
+    fontSize: 18,
+  },
+
+  footerPrevious: {
+    color: "#a3a3a3",
+    fontSize: 18,
+  },
+
+})
--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Image} from 'react-native';
+import AddToCart from './AddToCart';
+import OnlineShopping from './OnlineShopping ';
+import PaymentSuccessful from './PaymentSuccessful';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
+    <OnlineShopping/>
+      {/* <PaymentSuccessful/> */}
+      {/* <AddToCart/> */}
     </View>
   );
 }
@@ -14,8 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginVertical:30,
+    marginHorizontal:20,
   },
 });
