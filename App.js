7a56bed5dc5c701ef8e9cef140476084f1b7e9e2
Joel Alabi <joelalabi45@gmail.com>
2020-12-08 14:25:56 +0000

    added Navigation styles

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ import React from 'react';
 import { StyleSheet, Text, View ,Image} from 'react-native';
 import {createStackNavigator} from "@react-navigation/stack" 
 import {NavigationContainer} from '@react-navigation/native'
-
+import { AntDesign } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Fontisto } from '@expo/vector-icons';
 import AddToCart from './AddToCart';
 import OnlineShopping from './OnlineShopping ';
 import PaymentSuccessful from './PaymentSuccessful';
@@ -16,16 +18,32 @@ export default function App() {
 <Stack.Navigator>
 <Stack.Screen 
 options={{
-  title:"O shopping",
+  title:"Welcome To Online Shopping",
   headerStyle:{
      backgroundColor:"#8132a8"
   },
  headerTintColor:"white",
- headerRight:()=>(<Text style={{color:"white" ,marginRight:10}}>ICON</Text>)
+ headerRight:()=>(<Text style={{color:"white" ,marginRight:10}}><Fontisto name="shopping-basket-add" size={24} color="white" /></Text>)
 }}
 name="OnlineShopping" component={OnlineShopping}/>
-<Stack.Screen name="AddToCart" component={AddToCart}  />
-<Stack.Screen name="PaymentSuccessful" component={PaymentSuccessful}/>
+<Stack.Screen 
+options={{
+  headerStyle:{
+     backgroundColor:"#8132a8"
+  },
+ headerTintColor:"white",
+ headerRight:()=>(<Text style={{color:"white" ,marginRight:10}}><AntDesign name="shoppingcart" size={24} color="white" /></Text>)
+}}
+name="AddToCart" component={AddToCart}  />
+<Stack.Screen
+options={{
+  headerStyle:{
+     backgroundColor:"#8132a8"
+  },
+ headerTintColor:"white",
+ headerRight:()=>(<Text style={{color:"white" ,marginRight:10}}><MaterialIcons name="payment" size={24} color="white" /></Text>)
+}}
+ name="PaymentSuccessful" component={PaymentSuccessful}/>
 </Stack.Navigator>
 
    </NavigationContainer>
